e96fcfd013656cab127904561c2fa3047a161c37
mehmetalicetiner <cetinermehmetali@gmail.com>
2022-03-26 00:20:32 +0300

    Config file added

---
+++ config.py
@@ -0,0 +1,8 @@
+# ---- Configurable Variables ----
+commandPrefix = ','  # Prefix for commands
+channelSelectivity = True  # Ables/Disables operation on specific channel
+selectedChannel = 'uc'  # Specific channel's name
+
+# ---- NOT Configurable Variables ----
+guildID = [643857272216354866, 340277764907204608]  # IDs of guilds that allowed slash commands
+allowMessages = False
