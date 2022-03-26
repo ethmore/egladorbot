45e240dea487c377caf584ea766c25753ae65fb6
mehmetalicetiner <cetinermehmetali@gmail.com>
2022-03-26 15:35:03 +0300

    merge prepare

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import nextcord
 from nextcord.ext import commands
 
 # ---- Configurable Variables ----
-commandPrefix = ','  # Prefix for commands
+commandPrefix = '.'  # Prefix for commands
 channelSelectivity = True  # Ables/Disables operation on specific channel
-selectedChannel = 'uc'  # Specific channel's name
+selectedChannel = 'bot-commands'  # Specific channel's name
 
 # ---- NOT Configurable Variables ----
 guildID = [643857272216354866, 340277764907204608]  # IDs of guilds that allowed slash commands
