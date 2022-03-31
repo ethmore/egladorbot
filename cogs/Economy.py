1979da94dee311ddfdd25d1da1cfaadf3f42f579
mehmetalicetiner <cetinermehmetali@gmail.com>
2022-04-01 00:27:12 +0300

    coinflip base function added

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -2,7 +2,8 @@ import nextcord
 from nextcord.ext import commands
 import json
 import re
-
+import random
+import config
 
 class Economy(commands.Cog):
     def __init__(self, client):
@@ -10,7 +11,19 @@ class Economy(commands.Cog):
 
     @commands.command()
     async def addcoin(self, ctx, member: nextcord.Member, amount):
-       await ctx.send("WIP")
+        await ctx.send("WIP")
+
+    @commands.command(brief="`` {config.commandPrefix}coinflip `tails / heads` ``")
+    async def coinflip(self, ctx, guess=None):
+        flip_list = ["heads", "tails"]
+        result = random.choice(flip_list)
+        if guess is None:
+            await ctx.send(f"`` {config.commandPrefix}coinflip `tails / heads` ``")
+        else:
+            if result == guess:
+                await ctx.send("True")
+            else:
+                await ctx.send("Wrong")
 
 
 def setup(client):
