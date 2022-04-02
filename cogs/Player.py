e9503b761b7dc242b2522f9c43c46cad12f4a8e3
mehmetalicetiner <cetinermehmetali@gmail.com>
2022-04-02 18:39:59 +0300

    idle test

--- cogs/Player.py
+++ cogs/Player.py
@@ -5,6 +5,7 @@ from youtube_dl import *
 from nextcord import FFmpegPCMAudio
 from youtubesearchpython import VideosSearch
 import config
+import asyncio
 
 queues = {}
 
@@ -228,6 +229,15 @@ class Player(commands.Cog):
                 else:
                     await interaction.send("Provide a link or keyword/s")
 
+            while voice.is_playing():  # Checks if voice is playing
+                await asyncio.sleep(1)  # While it's playing it sleeps for 1 second
+            else:
+                await asyncio.sleep(15)  # If it's not playing it waits 15 seconds
+                while voice.is_playing():  # and checks once again if the bot is not playing
+                    break  # if it's playing it breaks
+                else:
+                    await voice.disconnect()  # if not it disconnects
+
         else:
             await interaction.send("Join a channel first")
 
@@ -437,6 +447,15 @@ class Player(commands.Cog):
                         await ctx.send("Player resumed")
                     else:
                         await ctx.send("Provide a link or keyword/s")
+
+                while voice.is_playing():  # Checks if voice is playing
+                    await asyncio.sleep(1)  # While it's playing it sleeps for 1 second
+                else:
+                    await asyncio.sleep(15)  # If it's not playing it waits 15 seconds
+                    while voice.is_playing():  # and checks once again if the bot is not playing
+                        break  # if it's playing it breaks
+                    else:
+                        await voice.disconnect()  # if not it disconnects
 
             else:
                 await ctx.send("Join a channel first")
